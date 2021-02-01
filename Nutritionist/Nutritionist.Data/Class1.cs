49f14953233a779d103ccd278f19c4bfc1263c5c
Mevlüt <mwltgr@gmail.com>
2021-02-01 13:39:38 +0300

    The project layers were created. These layers are API,Data,Model,Services,Web.

---
+++ Nutritionist/Nutritionist.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Nutritionist.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ Nutritionist/Nutritionist.Model/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Nutritionist.Model
+{
+    public class Class1
+    {
+    }
+}
